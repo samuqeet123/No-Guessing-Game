7d3a42b53e1ef78ad96cd3065f02a92dd2bb49a8
Abdul Muqeet Burki <164131418+samuqeet123@users.noreply.github.com>
2024-05-28 08:22:48 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,26 @@
+import inquirer from "inquirer"
+const answer = await inquirer.prompt([
+    {
+        type: "input",
+        name: "GuessedNumberByProgram",
+        message: "Choise the number between 0 to "
+    },
+    {
+        type: "input",
+        name: "GuessTheNumber",
+        message: "Enter the guessed number"
+    }
+])
+let guessed = parseInt(answer.GuessedNumberByProgram)
+let yourGuess = parseInt(answer.GuessTheNumber)
+// console.log(guessed)
+// console.log(yourGuess)
+
+let programGuessed = Math.floor(Math.random() * guessed)
+
+if (programGuessed == yourGuess) {
+    console.log("You Win")
+}
+else {
+    console.log(`You lose..The write answer was ${programGuessed}.. Try again`)
+}
